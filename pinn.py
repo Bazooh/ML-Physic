b68c3188e67e4f9b9f707c52c217a9838c6d3fa7
Jourdin Pierre <pierre.jourdin@student-cs.fr>
2025-04-15 10:17:55 +0200

    progress

--- pinn.py
+++ pinn.py
@@ -1,15 +1,15 @@
 from segmentation_models_pytorch import Unet
 import torch
+import torch.nn as nn
 import tqdm
-
-BATCH_SIZE = 16
-EPOCHS = 10
+from utils import derive_x, derive_y, pinn_loss
+from constants import *
 
 model = Unet(encoder_name="resnet18", 
              in_channels=1, 
              classes=1)
 
-def train(model, loss, optimizer, train_loader):
+def train(model, loss, optimizer, train_loader) -> None:
     for epoch in tqdm(range(EPOCHS)):
         model.train()
         for data in tqdm(train_loader):
@@ -21,7 +21,11 @@ def train(model, loss, optimizer, train_loader):
             optimizer.step()
             print(f"Epoch {epoch}, Train loss: {loss_value.item()}")
 
-def pinn_loss(u_grid_label, u_grid_predict, f_grid):
+def pinn_loss(u_grid_label: torch.Tensor, u_grid_predict: torch.Tensor, f_grid: torch.Tensor) -> float:
+    data_term = PINN_ * nn.MSELoss()(u_grid_label, u_grid_predict)
 
-    ddxu = 0
-    pass
+    ddxu = derive_x(derive_x(u_grid_predict))
+    ddyu = derive_y(derive_y(u_grid_predict))
+
+    physics_term = nn.MSELoss()(ddxu + ddyu, -f_grid)
+
