3a9b793504577491a048f52eb9c0b0f51869830f
Jourdin Pierre <pierre.jourdin@student-cs.fr>
2025-04-15 10:24:56 +0200

    pinn v1 done

--- pinn.py
+++ pinn.py
@@ -1,8 +1,8 @@
 from segmentation_models_pytorch import Unet
 import torch
 import torch.nn as nn
-import tqdm
-from utils import derive_x, derive_y, pinn_loss
+from tqdm import tqdm
+from utils import derive_x, derive_y
 from constants import *
 
 model = Unet(encoder_name="resnet18", 
@@ -22,10 +22,17 @@ def train(model, loss, optimizer, train_loader) -> None:
             print(f"Epoch {epoch}, Train loss: {loss_value.item()}")
 
 def pinn_loss(u_grid_label: torch.Tensor, u_grid_predict: torch.Tensor, f_grid: torch.Tensor) -> float:
-    data_term = PINN_ * nn.MSELoss()(u_grid_label, u_grid_predict)
+    data_term = PINN_LOSS_DATA_PROP * nn.MSELoss()(u_grid_label, u_grid_predict)
 
     ddxu = derive_x(derive_x(u_grid_predict))
     ddyu = derive_y(derive_y(u_grid_predict))
 
-    physics_term = nn.MSELoss()(ddxu + ddyu, -f_grid)
+    physics_term = PINN_LOSS_PHYSICS_PROP * nn.MSELoss()(ddxu + ddyu, -f_grid)
+
+    true_corners = torch.zeros(2,2)
+    predicted_corners = torch.tensor([[u_grid_predict[0,0], u_grid_predict[0,-1]],
+                                       [u_grid_predict[-1,0], u_grid_predict[-1,-1]]])
+    border_term = PINN_LOSS_BORDER_PROP * nn.MSELoss()(true_corners, predicted_corners)
+
+    return data_term + physics_term + border_term
 
